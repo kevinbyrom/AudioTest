51707c4b4943b1cb5a8bfdc68d73e456f14a598a
kbyrom <kbyrom@riotgames.com>
2018-07-09 16:40:45 -0700

    initial commit

---
+++ AudioTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioTest
+{
+    class Program
+    {
+        static private WaveOut waveOut;
+
+        static void Main(string[] args)
+        {
+            StartStopSineWave();
+            Console.ReadLine();
+            StartStopSineWave();
+        }
+
+
+        static private void StartStopSineWave()
+        {
+            if (waveOut == null)
+            {
+                var sineWaveProvider = new SineWaveProvider32();
+                sineWaveProvider.SetWaveFormat(16000, 1); // 16kHz mono
+                sineWaveProvider.Frequency = (float)440;
+                sineWaveProvider.Amplitude = 0.25f;
+                waveOut = new WaveOut();
+                waveOut.Init(sineWaveProvider);
+                waveOut.Play();
+            }
+            else
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+    }
+}
---
+++ AudioTest/WaveProvider32.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+using System.Diagnostics;
+
+
+namespace AudioTest
+{
+    public abstract class WaveProvider32 : IWaveProvider
+    {
+        private WaveFormat waveFormat;
+
+        public WaveProvider32()
+            : this(44100, 1)
+        {
+        }
+
+        public WaveProvider32(int sampleRate, int channels)
+        {
+            SetWaveFormat(sampleRate, channels);
+        }
+
+        public void SetWaveFormat(int sampleRate, int channels)
+        {
+            this.waveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channels);
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            WaveBuffer waveBuffer = new WaveBuffer(buffer);
+            int samplesRequired = count / 4;
+            int samplesRead = Read(waveBuffer.FloatBuffer, offset / 4, samplesRequired);
+            return samplesRead * 4;
+        }
+
+        public abstract int Read(float[] buffer, int offset, int sampleCount);
+
+        public WaveFormat WaveFormat
+        {
+            get { return waveFormat; }
+        }
+    }
+
+
+    public class SineWaveProvider32 : WaveProvider32
+    {
+        int sample;
+
+        public SineWaveProvider32()
+        {
+            Frequency = 1000;
+            Amplitude = 0.25f; // let's not hurt our ears            
+        }
+
+        public float Frequency { get; set; }
+        public float Amplitude { get; set; }
+
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+            Debug.WriteLine($"{offset} - {sampleCount}");
+            Random rnd = new Random();
+
+            int sampleRate = WaveFormat.SampleRate;
+            for (int n = 0; n < sampleCount; n++)
+            {
+                var rndVal = rnd.Next(3);
+
+                //var val = (float)(Amplitude * Math.Sin((rndVal * Math.PI * sample * Frequency) / sampleRate));
+                var val = (float)(Amplitude * Math.Sin((Math.PI * sample * Frequency) / sampleRate));
+
+                buffer[n + offset] = val; //(float)(Amplitude * Math.Sin((2 * (Math.PI / 4) * sample * Frequency) / sampleRate));
+                
+                sample++;
+                if (sample >= sampleRate) sample = 0;
+            }
+            return sampleCount;
+        }
+    }
+}
